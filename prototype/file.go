bb914b1850041d0f0c7b12ae5933d2c7e8a40158
muhmunt <muhmunt@gmail.com>
2023-01-14 10:07:47 +0700

    implement prototype

---
+++ prototype/file.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+type File struct {
+	name string
+}
+
+func (f *File) print(indentation string) {
+	fmt.Println(indentation + f.name)
+}
+
+func (f *File) clone() Inode {
+	return &File{name: f.name + "_clone"}
+}
